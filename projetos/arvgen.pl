49c705feb23164fdb3d21717a35defa2ade086d8
Thiago Martins <thiago.martins1112@gmail.com>
2025-04-12 21:22:55 -0300

    chore: adicionado conteúdo conforme primeira parte do material.

--- projetos/arvgen.pl
+++ projetos/arvgen.pl
@@ -1,17 +1,60 @@
+%// 
+% pai(X,Y). == fato
+% 
+% . = fim de uma cláusula.
+% , = and.
+% ; = or.
+% :- = if.
+% \= = diferente de
+% 
+%//
+
 % fatos
-pai(marcelo, roberto).
-pai(marcelo, carlos).
-pai(carlos, caio).
-pai(jeremias, maria).
+pai(joao,joaquim).
+pai(joao,lucas).
+pai(pedro,silvia).
+pai(joaquim,bruno).
 
-mae(juliana, roberto).
-mae(juliana, carlos).
-mae(thais, maria).
-mae(maria, caio).
+mae(maria,joaquim).
+mae(maria,lucas).
+mae(laura,silvia).
+mae(silvia,bruno).
 
-% regras
-irmao(X,Y) :- (pai(P,X) , pai(P,Y) , X \= Y) ; (mae(M,X) , mae(M,Y), X \= Y).
 
-avo(X,Y) :- ((pai(X,P) ; mae(X,P)), (pai(P,Y) ; mae(P,Y))).
+% //
+% todas as consultas diretas irão responder apenas true ou false, mas caso tenhamos uma
+% consulta com incógnita, prolog irá responder os valores que fazem a equação se tornar
+% verdadeira, ou responderá diretamente false, caso não haja nenhuma.
+% 
+% Ex: ?-pai(joao,joaquim). == true
+% 	  ?-pai(X,silvia) == pedro
+% 	  ?-pai(joao,X) == joaquim && lucas
+% 	  ?-pai(X,Y) == mostrará todos os pares possíveis de pais e filhos, conforme fatos.
+% 
+% É importante ressaltar que somente fatos já podem ser utilizados para consultas mais 
+% complexas, por exemplo, é possível descobrir o avô de Bruno a partir de:
+% 
+%  ?-pai(Y,bruno) , pai(X,Y) == joao, pois é pai do pai de bruno.
+% //
 
-tio(X,Y) :- (irmao(X,P) , pai(P,Y)) ; (irmao(X,M) , mae(M,Y)).
+
+
+% //
+% Exercícios:
+% 1. Sejam pai e mãe as relações como definidas no programa exemplo anterior. Qual será a resposta
+% de prolog para as seguintes questões:
+% 
+% • pai(joao, X). == joaquim && lucas.
+% 
+% • pai(X,joaquim). == joao.
+% 
+% • pai(joao,X), pai(X,bruno). == joaquim.
+% 
+% 2. Formule em prolog as seguintes consultas sobre as relações pai e mãe do programa anterior.
+% 
+% • Quem é o pai de Bruno?  == pai(X,bruno).
+% 
+% • Maria tem filhos? == mae(maria, _).
+% 
+% • Quem é o avô de Bruno? == pai(Y,bruno), pai(X,Y).
+% //
